27043ede9ac1a318ed6369114b3a7407db629fbf
Alex Afshar <contact@alexafshar.com>
2020-04-12 02:55:09 -0400

    Added ability to create new Stock.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate dotenv;
 pub mod models;
 pub mod schema;
 
+use self::models::{NewStock, Stock};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
@@ -17,4 +18,34 @@ pub fn establish_connection() -> PgConnection {
 
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_stock<'a>(
+    conn: &PgConnection,
+    ticker: &'a str,
+    name: Option<&'a str>,
+    description: Option<&'a str>,
+) -> Stock {
+    use schema::stocks;
+
+    // If the ticker is greater than 4 characters,
+    // let's just panic for now.
+    if ticker.len() > 4 {
+        panic!("Ticker must be 4 characters or less!");
+    }
+
+    // No need to match on Some or None for Option<T> values
+    // as Diesel will take care of this for us.
+    let new_stock = NewStock {
+        ticker,
+        name,
+        description,
+    };
+
+    // Diesel will supply DEFAULT values for and None
+    // if the field is marked NULLABLE
+    diesel::insert_into(stocks::table)
+        .values(&new_stock)
+        .get_result(conn)
+        .expect("Error saving new stock")
 }
--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,17 @@
+use super::schema::stocks;
+
 #[derive(Queryable)]
 pub struct Stock {
     pub id: i32,
     pub ticker: String,
     pub name: Option<String>,
     pub description: Option<String>,
+}
+
+#[derive(Insertable)]
+#[table_name = "stocks"]
+pub struct NewStock<'a> {
+    pub ticker: &'a str, // This has a max size of 4.
+    pub name: Option<&'a str>,
+    pub description: Option<&'a str>,
 }
